b554a4a5acd9f1183de68463bb56ba1b38de2b5b
Jessica <jbc7ag@gmail.com>
2020-08-15 21:33:20 -0500

    add navigation graph and change layouts design

---
+++ app/src/main/java/com/jbc7ag/luckypinata/myAdviceFragment.kt
@@ -0,0 +1,18 @@
+package com.jbc7ag.luckypinata
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class myAdviceFragment: Fragment(){
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        return inflater.inflate( R.layout.fragment_my_advice, container, false)
+    }
+
+}
